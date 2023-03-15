430c57542e247d27ccb459a7e6c9ec8a34a426ee
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-15 01:21:48 -0300

    feat: função request api

---
+++ src/services/requestApi.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+export const requestApi = async <T> ({method, url, headers = {}, params = {}, data = {}}: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
+  const baseURL = 'http://localhost:3000/api/v1';
+  try {
+    const response = await axios({method, baseURL, url, headers, params, data});
+    return response;
+  } catch(err: any) {
+    throw err.response;
+  }
+};
