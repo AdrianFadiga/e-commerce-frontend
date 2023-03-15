d6bf22203eed2f12b6d288dd18b54b82136b78a3
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-14 23:19:04 -0300

    feat: install crakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Login from './pages/Login';
 import { Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+    <ChakraProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </ChakraProvider>
   );
 }
 
