1f0e9729f77cc80df0b2cfcb48424a0cdde21a30
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-19 09:28:29 -0300

    fix: rota da função requestApi

--- src/services/requestApi.ts
+++ src/services/requestApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 export const requestApi = async <T> ({method, url, headers = {}, params = {}, data = {}}: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
-  const baseURL = 'http://localhost:3000/api/v1';
+  const baseURL = 'http://localhost:3000/api';
   try {
     const response = await axios({method, baseURL, url, headers, params, data});
     return response;
