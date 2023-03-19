e808e8db3ba7e4b85b1b02ccafebe5ff414e5013
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-19 09:26:58 -0300

    feat: product card component

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,53 @@
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
+interface ProductCard {
+  name: string
+  description: string
+  price: number
+}
+
+const ProductCard: React.FC<ProductCard> =({name, description, price}) => {
+  return (
+    <Card maxW="sm">
+      <CardBody>
+        <Image
+          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          borderRadius="lg"
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{name}</Heading>
+          <Text>
+            {description}
+          </Text>
+          <Text color="blue.600" fontSize="2xl">
+            {price}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing="2">
+          <Button variant="solid" colorScheme="blue">
+            Buy now
+          </Button>
+          <Button variant="ghost" colorScheme="blue">
+            Add to cart
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProductCard;
