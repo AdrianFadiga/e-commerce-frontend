d02b413d5428d8da8a839b2bf62a25b91a1c71fb
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-19 09:25:47 -0300

    fix: home na rota "/"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <ChakraProvider>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </ChakraProvider>
   );
