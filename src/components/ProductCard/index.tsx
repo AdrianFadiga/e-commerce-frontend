d300d67af1ab00fec381d37ae13fc11cc8bc5a69
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-03-22 00:33:45 -0300

    feat: product_card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -10,14 +10,28 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
+import { useState } from 'react';
+
+import {BsCartPlusFill} from 'react-icons/bs';
+import { requestApi } from '../../services';
 
 interface ProductCard {
   name: string
   description: string
   price: number
+  product_id: string
 }
 
-const ProductCard: React.FC<ProductCard> =({name, description, price}) => {
+const ProductCard: React.FC<ProductCard> =({name, description, price, product_id}) => {
+  const [quantity, setQuantity] = useState<number>(1);
+  const addToCart = async () => {
+    try {
+      const accessToken = JSON.parse(localStorage.getItem('accessToken') as string);
+      await requestApi({method: 'PUT', url: '/v1/shopping_carts', headers: {...accessToken}, data: {product_id, quantity}});
+    } catch (err: any) {
+      console.log(err.data);
+    }
+  };
   return (
     <Card maxW="sm">
       <CardBody>
@@ -38,11 +52,30 @@ const ProductCard: React.FC<ProductCard> =({name, description, price}) => {
       <Divider />
       <CardFooter>
         <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Buy now
+          <Button 
+            variant="solid" 
+            colorScheme="blue" 
+            size="sm"
+            onClick={() => setQuantity(quantity + 1)}>
+            +
           </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
+          <span>{quantity}</span>
+          <Button 
+            variant="solid" 
+            colorScheme="blue" 
+            size="sm"
+            onClick={() => {if (quantity > 1) setQuantity(quantity - 1);}}
+          >
+            -
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button 
+            variant="ghost" 
+            colorScheme="blue"
+            onClick={() => addToCart()}
+          >
+            <BsCartPlusFill />
           </Button>
         </ButtonGroup>
       </CardFooter>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,6 +53,7 @@ const Home: React.FC = () => {
                 name={name}
                 price={price}
                 description={description}
+                product_id={id}
               />
             ))}
           </SimpleGrid>
